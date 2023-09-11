7e240857810f606d03c670abac65685b12ffd9cb
Ergi004 <ergilama98@gmail.com>
2023-09-11 04:40:47 -0700

    left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (leftNode == NULL)
 		return (NULL);
-
+	
+	leftNode->n = value;
 	leftNode->left = NULL;
 	leftNode->right = NULL;
 	leftNode->parent = NULL;
