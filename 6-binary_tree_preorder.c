5eb0045938d58ae9057b64eb7a2384c9b088ab77
Ergi004 <ergilama98@gmail.com>
2023-09-11 06:02:27 -0700

    The pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Does the pre-order traversal
+ * @tree: pointer to root
+ * @func: ponter to function
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL && func == NULL)
+		return (0);
+
+	if (tree != NULL)
+		func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
