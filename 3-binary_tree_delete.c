0bddc17b66491098ebbed8c5004435ea1bc68681
Ergi004 <ergilama98@gmail.com>
2023-09-11 05:30:29 -0700

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a node
+ * @tree: pointer
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	if (tree->lefi != NULL)
+	{
+		binary_tree_delete(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		binary_tree_delete(tree->right);
+	}
+
+	free(tree);
+}
