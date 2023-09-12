f4c5f1621f844ae4c9cf5d07f33500f421c04626
Ergi004 <ergilama98@gmail.com>
2023-09-12 02:20:21 -0700

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - Prints the size of the tree
  * @tree: The tree
- * Return: Function
+ * Return: Functio
  */
 
 
@@ -12,4 +12,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + 1 binary_tree_size(tree->right));
+}
