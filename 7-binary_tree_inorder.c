40e465a097d5fff8be25729628a134b2c8d61041
Ergi004 <ergilama98@gmail.com>
2023-09-12 01:03:08 -0700

    betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - Does the in-order traversal
+ * @tree: Pointer
+ * @func: Pointer to a function
+ * Return: Nothing.
+ */
+
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
