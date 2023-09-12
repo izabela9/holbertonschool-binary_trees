141ce2219b320aaa2db2cce6dd87f0dbedfcd873
Ergi004 <ergilama98@gmail.com>
2023-09-12 08:58:04 -0700

    done

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "proxy_height.c"
 
 /**
  * binary_tree_sibling - Checks if the node has siblings
@@ -10,11 +9,10 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-	       return (0);
+		return (0);
 
 	if (node->parent->left == node)
 		return (node->parent->right);
 	else
 		return (node->parent->left);
-
-}	
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
-#include "proxy_height.c"
-
 /**
- * binary_tree_uncle - Checks if the ode has uncles
- * @node: Node to be checked
- * Return: Nothing
+ * binary_tree_uncle - Find the uncle of a node.
+ * @node: A pointer to the node for which to find the uncle.
+ * Return: A pointer to the uncle node, or NULL if node is NULL,
+ * the parent is NULL, or if there is no uncle.
  */
-
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent == node->parent->parent->left)
+	{
 		return (node->parent->parent->right);
+	}
 	else
 		return (node->parent->parent->left);
 }
